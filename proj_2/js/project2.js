d9fd0175b9027b8857a58fc5984731f0a3bdc9f1
Chiwis1 <168983460+Chiwis1@users.noreply.github.com>
2024-10-23 20:41:25 -0500

    Add files via upload

---
+++ proj_2/js/project2.js
@@ -0,0 +1,35 @@
+// Name: Jeovany Velazquez-Gonzalez Date: 10/23/24
+
+// Section 1
+let myName = "Jeovany Velazquez-Gonzalez";
+let para1 = document.getElementById("p1");
+
+para1.textContent = myName;
+
+// Section 2
+let n1 = 5;
+let n2 = 7;
+let numberSum = n1 + n2;
+
+document.getElementById("p2").textContent = numberSum;
+
+// Section 3
+let numberMult = n1 * n2;
+
+document.getElementById("p3").textContent =  numberMult;
+
+// Section 4
+let myNameAddNum = myName + n1;
+
+document.getElementById("p4").textContent = myNameAddNum;
+
+// Section 5
+let myNameMultNum = myName * n1;
+
+document.getElementById("p5").textContent = myNameMultNum;
+
+// Section 6
+let age = 25;
+let ageCompare = age >= numberMult;
+
+document.getElementById("p6").textContent = ageCompare;
