ee920ac91c51b750b850443ec20f16de2c9c1051
Chiwis1 <168983460+Chiwis1@users.noreply.github.com>
2024-11-06 21:51:17 -0600

    Add files via upload

---
+++ proj_3/js/project3.js
@@ -0,0 +1,36 @@
+// Name: Jeovany Velazquez-Gonzalez Date: 11/6/24
+
+// Step 1
+let fullName = "Jeovany Velazquez-Gonzalez";
+
+console.log(fullName);
+
+// Step 2
+let salaryHope = 55000;
+
+console.log(salaryHope);
+
+// Step 3
+let vetStatus = false;
+
+console.log(vetStatus);
+
+// Step 4
+let friendsName = ['joe', 'james', 'jack'];
+
+console.log(friendsName);
+
+// Step 5
+let fSalary = [60000, 75000, 70000];
+
+console.log(fSalary);
+
+// Step 6
+let friendFour = new Object();
+
+friendFour.name = 'jacob';
+friendFour.lastname = 'jhonson';
+friendFour.das = 80000;
+
+console.log(friendFour);
+
